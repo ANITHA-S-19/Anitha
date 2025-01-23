bde23932d84ed5ad6751559979e7d5b563416eed
ANITHA-S-19 <anithasrinivasan148@gmail.com>
2025-01-23 10:20:47 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+function handleLogin()
+{
+    const username=document.getElementById('user').value ;
+    const password=document.getElementById('pass').value ;
+    const error=document.getElementById('error') 
+
+    const loginData=[
+        {username:"Apsala",password:"pass123"},
+        {username:"Abi",password:"123pass"},
+        {username:"Madhu",password:"123@4"},
+    ]
+    const isValid=loginData.some((user)=>user.username===username && user.password===password)
+    if (isValid)
+    {
+        window.location.href='Flexbox.html'
+        error.innerText="login Successful";
+    }
+    else{
+        error.innerText="Invalid Credentials";
+    }
+}
